04ea100a642cd01a9d3a4b9ec1ec8459e6f948da
Jessie <jess.bell@me.com>
2017-09-13 20:58:54 -0700

    add new content to

---
+++ practice/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // $(".clickable").click(function() {
+  //   $("#walrus-showing").toggle();
+  //   $("#walrus-hidden").toggle();
+  //   $("#somethingother").toggle();
+
+  $(".clickable").click(function() {
+    $("#army").toggle();
+  });
+});
